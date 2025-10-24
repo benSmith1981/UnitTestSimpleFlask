136e76554bfb1fa46383944e4766c79303fd419b
Ben Smith <benSmith1981@users.noreply.github.com>
2025-10-24 12:58:06 +0100

    updated to not insert into real db

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import sqlite3, os
 app = Flask(__name__)
 app.secret_key = "supersecret"
 
+
 # --- Create Database ---
 def init_db():
-    db_path = app.config.get("DATABASE", "database.db")
-
+    db_path = app.config.get("DATABASE", "database.db") 
     if not os.path.exists(db_path):
-        conn = sqlite3.connect(app.config.get("DATABASE", "database.db"))
+        conn = sqlite3.connect(db_path)
         c = conn.cursor()
         c.execute("""
             CREATE TABLE submissions (
